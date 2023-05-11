3efa44bdd4f3bda5c6b715534c200bf681307b0c
JaeHyeonSim <peanutandnuts@gmail.com>
2023-05-11 11:31:48 +0900

    [Add]first commit

---
+++ src/main/java/com/frog/frogbanchan/FrogbanchanApplication.java
@@ -0,0 +1,13 @@
+package com.frog.frogbanchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogbanchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrogbanchanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frog/frogbanchan/FrogbanchanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frog.frogbanchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogbanchanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
