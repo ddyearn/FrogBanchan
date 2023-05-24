1ad69945cbdfc3694e4c38392424bd5a74abe7d1
Nayeon Kim <skdus3373@naver.com>
2023-05-25 02:51:17 +0900

    [feat] ListTeamController 구현

---
+++ src/main/java/com/frog/frogbanchan/controller/team/ListTeamController.java
@@ -0,0 +1,31 @@
+package com.frog.frogbanchan.controller.team;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.frog.frogbanchan.controller.UserSession;
+import com.frog.frogbanchan.service.FrogBanchanFacade;
+
+@Controller
+@SessionAttributes("userSession")
+public class ListTeamController { 
+
+	private FrogBanchanFacade frogbanchan;
+
+	@Autowired
+	public void setFrogbanchan(FrogBanchanFacade frogbanchan) {
+		this.frogbanchan = frogbanchan;
+	}
+
+	@RequestMapping("/team/list.do")
+	public ModelAndView handleRequest(
+			@ModelAttribute("userSession") UserSession userSession) throws Exception {
+		String username = userSession.getUser().getUsername();
+		
+		return new ModelAndView("/team/list", "teamList", frogbanchan.findTeamList(username));
+	}
+}
