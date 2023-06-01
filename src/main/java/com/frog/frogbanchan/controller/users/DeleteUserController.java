93bbfef03cc71298c103dd6d9319b84a563745b1
JaeHyeonSim <peanutandnuts@gmail.com>
2023-06-02 02:22:18 +0900

    [feat] delete user 구현

---
+++ src/main/java/com/frog/frogbanchan/controller/users/DeleteUserController.java
@@ -0,0 +1,42 @@
+package com.frog.frogbanchan.controller.users;
+
+import com.frog.frogbanchan.controller.UserSession;
+import com.frog.frogbanchan.domain.Team;
+import com.frog.frogbanchan.service.FrogBanchanFacade;
+import com.frog.frogbanchan.service.WithdrawalUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+public class DeleteUserController {
+
+    private FrogBanchanFacade frogBanchan;
+    @Autowired
+    public void setFrogBanchan(FrogBanchanFacade frogBanchan) {
+        this.frogBanchan = frogBanchan;
+    }
+
+    private WithdrawalUserService withdrawalService;
+    @Autowired
+    public void setWithdrawalService(WithdrawalUserService withdrawalService) {
+        this.withdrawalService = withdrawalService;
+    }
+
+    @RequestMapping("/user/delete")
+    public String handleRequest(@SessionAttribute("userSession") UserSession userSession,
+                                HttpSession session) throws Exception {
+
+        String user = userSession.getUser().getUsername();
+        withdrawalService.withdrawal(user);
+        session.removeAttribute("userSession");
+
+        return "redirect:/login";
+    }
+
+
+}
