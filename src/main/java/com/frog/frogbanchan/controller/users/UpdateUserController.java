92e2734f720b05993cb3f9d5ed5660d33800e330
JaeHyeonSim <peanutandnuts@gmail.com>
2023-06-02 02:20:29 +0900

    [feat] myPage 구현

---
+++ src/main/java/com/frog/frogbanchan/controller/users/UpdateUserController.java
@@ -0,0 +1,71 @@
+package com.frog.frogbanchan.controller.users;
+
+import com.frog.frogbanchan.controller.UserSession;
+import com.frog.frogbanchan.domain.Users;
+import com.frog.frogbanchan.service.FrogBanchanFacade;
+import com.frog.frogbanchan.service.validator.UserFormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.util.WebUtils;
+
+@Controller
+@RequestMapping("/user/modify")
+public class UpdateUserController {
+    @Value("/user/userModify")
+    private String FORM_VIEW;
+    @Value("redirect:/user/myPage")
+    private String RESULT_VIEW;
+
+    private FrogBanchanFacade frogBanchan;
+
+    @Autowired
+    public void setFrogBanchan(FrogBanchanFacade frogBanchan) {
+        this.frogBanchan = frogBanchan;
+    }
+
+    @Autowired
+    private UserFormValidator validator;
+    public void setValidator(UserFormValidator validator) {
+        this.validator = validator;
+    }
+
+    @ModelAttribute("userForm")
+    public UserForm formBacking(@SessionAttribute("userSession") UserSession userSession)
+            throws Exception {
+        UserForm userForm = new UserForm(userSession.getUser());
+        userForm.setConfirmPassword(userSession.getUser().getPassword());
+        return userForm;
+    }
+
+    @GetMapping
+    public String showForm() {
+        return FORM_VIEW;
+    }
+
+    @PostMapping
+    public String onSubmit(
+            /*@Valid*/ @ModelAttribute("userForm") UserForm userForm,
+                       BindingResult bindingResult, Model model,
+                       SessionStatus sessionStatus) {
+        System.out.println("command 객체: " + userForm);
+
+        validator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return FORM_VIEW;
+        }
+
+        Users user = userForm.getUser();
+        frogBanchan.updateUser(user);
+        model.addAttribute("user", user);
+
+        return RESULT_VIEW;
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/user/userModify.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>정보 수정</title>
+</head>
+<body>
+    <form:form modelAttribute="userForm" method="post">
+        <label for="username">아이디</label>:
+        <form:input path="user.username" readonly="true" />
+        <br/>
+        <label for="password">비밀번호</label>:
+        <form:password path="user.password" showPassword="true"/>
+        <form:errors path="user.password"/>
+        <br/>
+        <label for="confirmPassword">비밀번호 확인</label>:
+        <form:password path="confirmPassword" showPassword="true"/>
+        <form:errors path="confirmPassword"/>
+        <br/>
+        <label for="name">이름</label>:
+        <form:input path="user.name" />
+        <form:errors path="user.name"/>
+        <br/>
+        <label for="nickname">닉네임</label>:
+        <form:input path="user.nickname" />
+        <form:errors path="user.nickname"/>
+        <br/>
+        <label for="phone">전화번호</label>:
+        <form:input path="user.phone" />
+        <form:errors path="user.phone"/>
+        <br/>
+        <label for="email">이메일</label>:
+        <form:input path="user.email" />
+        <form:errors path="user.email"/>
+        <br/>
+        <label for="residentNo">주민번호 7자리</label>:
+        <form:input path="user.residentNo" />
+        <form:errors path="user.residentNo"/>
+        <br/>
+        <label for="address">주소</label>:
+        <form:input path="user.address" />
+        <form:errors path="user.address"/>
+        <br/>
+
+        </br>
+        <input type="submit" value="수정하기" />
+    </form:form>
+    <button type="button" onClick="location.href='/user/delete'">탈퇴하기</button>
+</body>
+</html>
