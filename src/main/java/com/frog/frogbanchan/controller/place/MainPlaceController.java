66695ac308534dc02e0cdb56c6828d3ea3f9277f
JaeHyeonSim <peanutandnuts@gmail.com>
2023-06-02 03:29:27 +0900

    [update] place ui

--- src/main/java/com/frog/frogbanchan/controller/place/MainPlaceController.java
+++ src/main/java/com/frog/frogbanchan/controller/place/MainPlaceController.java
@@ -1,9 +1,12 @@
 package com.frog.frogbanchan.controller.place;
 
+import com.frog.frogbanchan.controller.PlaceSession;
+import com.frog.frogbanchan.domain.Place;
 import com.frog.frogbanchan.service.FrogBanchanFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,8 +20,12 @@ public class MainPlaceController {
     }
 
     @RequestMapping("/place/main")
-    public ModelAndView handleRequest1() throws Exception {
+    public ModelAndView handleRequest1(@SessionAttribute("placeSession") PlaceSession placeSession)
+                                      throws Exception {
         ModelAndView mav = new ModelAndView("/place/mainForPlace");
+
+        Place place = placeSession.getPlace();
+        mav.addObject("place", place);
 
         return mav;
     }
