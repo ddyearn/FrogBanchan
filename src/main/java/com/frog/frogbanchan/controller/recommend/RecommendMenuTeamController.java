b82091098f42a6106f30085e7ef7adf121f7c49a
JaeHyeonSim <peanutandnuts@gmail.com>
2023-06-15 14:08:41 +0900

    [refactor] split recommend controller

--- src/main/java/com/frog/frogbanchan/controller/recommend/RecommendMenuTeamController.java
+++ src/main/java/com/frog/frogbanchan/controller/recommend/RecommendMenuTeamController.java
@@ -14,43 +14,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// recommend team, personal view 처리를 위한 임시 컨트롤러 (실제 구현 시 삭제 !!)
 @Controller
-@SessionAttributes("userSession")
-public class TestRecommendController {
+public class RecommendMenuTeamController {
 
     private FrogBanchanFacade frogBanchan;
 
     @Autowired
     public void setFrogBanchan(FrogBanchanFacade frogBanchan) {
         this.frogBanchan = frogBanchan;
-    }
-
-    @RequestMapping("/recommend/personal")
-    public ModelAndView test1(
-            @ModelAttribute("userSession") UserSession userSession
-    ) throws Exception {
-        ModelAndView mav = new ModelAndView("/user/recommendPersonal");
-        String user = userSession.getUser().getUsername();
-        List<String> hateList = frogBanchan.findTagsByUsername(user);
-        mav.addObject("hateList", hateList);
-        return mav;
-    }
-
-    @RequestMapping("/recommend/personal/test1")
-    public ModelAndView test4(
-            @ModelAttribute("userSession") UserSession userSession,
-            @RequestParam("likeTags") String likeTags,
-            @RequestParam("hateTags") String hateTags
-    ) throws Exception {
-        ModelAndView mav = new ModelAndView("/user/recommendTest1");
-
-        List<String> hateList = new ArrayList<>(Arrays.asList(hateTags.split(",")));
-        mav.addObject("hateList", hateList);
-        List<String> likeList = new ArrayList<>(Arrays.asList(likeTags.split(",")));
-        mav.addObject("likeList", likeList);
-
-        return mav;
     }
 
     @RequestMapping("/recommend/team")
